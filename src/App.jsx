2b3e1af71157de4ee862d2b0f1a7d7fe81476872
Roman Zaichenko <romanzaichenko33@gmail.com>
2025-07-14 14:33:28 +0300

    Create App component.

--- src/App.jsx
+++ src/App.jsx
@@ -18,5 +18,40 @@ function App() {
     setWord(temporary)
   }
 
+  return (
+    <div className="App">
+      <h1>QR Code Generator</h1>
+      <div className="input-box">
+        <div className="gen">
+          <input type='text' onChange={
+            (e) => setTemporary(e.target.value)}
+            placeholder="Enter text to encode"/>
 
+          <button className="btn"  onClick={handleClick}>
+            Generate
+          </button>
+        </div>
+        <div className="extra">
+          <h5>Background Color:</h5>
+          <input type="color" onChange={(e)=>
+            setBgColor(e.target.value.substring(1))}/>
+
+          <h5>Dimension:</h5>
+          <input type="range" min="200" max="600" value={size}
+                 onChange={(e)=> {
+                   setSize(e.target.value)}
+                 }/>
+        </div>
+      </div>
+
+      <div className="output-box">
+        <img src={qrCode} alt="QR Code"/>
+        <a href={qrCode} download="QRCode">
+          <button className="btn">Download</button>
+        </a>
+      </div>
+    </div>
+  )
 }
+
+export default App
